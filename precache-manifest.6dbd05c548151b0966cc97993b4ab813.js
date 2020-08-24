efb0325035681563c56cb9e7f651bc03da469f4d
SlavkoPar <slavko.parezanin@gmail.com>
2020-08-24 14:26:19 +0200

    Updates

---
+++ precache-manifest.6dbd05c548151b0966cc97993b4ab813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e8f3d5ec45bf6c47a2ecd213f40f33",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "9f3a71f1d1ab0d616f31",
+    "url": "/rick-and-morty/static/css/main.978ddfe7.chunk.css"
+  },
+  {
+    "revision": "0c16755bee0128babb6b",
+    "url": "/rick-and-morty/static/js/2.97674f4f.chunk.js"
+  },
+  {
+    "revision": "150ff86ada6d4117f6d430f6acba325e",
+    "url": "/rick-and-morty/static/js/2.97674f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3a71f1d1ab0d616f31",
+    "url": "/rick-and-morty/static/js/main.3e53c594.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  }
+]);
